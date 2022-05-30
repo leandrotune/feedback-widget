645282d9ecb22543bf544d1fd19a5f5da2b234a2
Leandro Antunes <lele.tune@gmail.com>
2022-05-29 22:29:34 -0300

    deploy

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -35,6 +35,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
             screenshot,
         });
 
+        setIsSendingFeedback(false);
         onFeedbackSent()
     }
 
@@ -76,6 +77,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
                         {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
+
                     </button>
                 </footer>
             </form>
